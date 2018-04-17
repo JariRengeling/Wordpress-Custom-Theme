13c1704ff841c48e721813761f2a756fa6b161c3
Jari Rengeling <jarirengeling@outlook.com>
2018-04-17 15:50:57 +0200

    Add admin page
    Wordpress | Add admin page

--- wp-content/themes/sunset-theme/functions.php
+++ wp-content/themes/sunset-theme/functions.php
@@ -1,3 +1,3 @@
 <?php
 	
-require get_template_directory() . '/inc/function-admin.php';
+require get_template_directory() . '/inc/function-admin.php'; 
--- wp-content/themes/sunset-theme/inc/function-admin.php
+++ wp-content/themes/sunset-theme/inc/function-admin.php
@@ -11,7 +11,7 @@
 
 function sunset_add_admin_page() {
 	
-	add_menu_page( 'Sunset Theme Options', 'Sunset', 'manage_options', 'alecaddd-sunset', 'sunset_theme_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110 );
+	add_menu_page( 'Sunset Theme Options', 'Sunset', 'manage_options', 'alecaddd-sunset', 'sunset_theme_create_page', 'dashicons-admin-customizer', 110 );
 	
 }
 add_action( 'admin_menu', 'sunset_add_admin_page' );
